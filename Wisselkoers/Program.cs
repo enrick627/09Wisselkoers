6a306b12f4920d5eb6889cd4b3774c7ff6a51c95
Enrick De Munter <enrick.demunter@campus.romerocollege.be>
2019-10-18 10:33:33 +0200

    Add project files.

---
+++ Wisselkoers/Program.cs
@@ -0,0 +1,24 @@
+﻿//Enrick De Munter 5ITN nr4 18/10/2019
+/*Opgave
+ * maak een console toepassing waarbij een bedrag in euro omgerekend wordt naar dollar. 
+ * De wisselkoers van 1.00 Eur = 1.0975 Dollar.Het resultaat moe ter als volgt uitzien
+ * 
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wisselkoers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
